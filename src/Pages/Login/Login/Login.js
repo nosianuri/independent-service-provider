5ae88f49b428a3ab53c04810b07f0497473693ec
nosianuri <nurinosia@gmail.com>
2022-04-18 23:58:29 +0600

    set hosting p[age

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,14 @@
+import { async } from '@firebase/util';
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
+import Loading from '../../Shared/Loading/Loading';
+import SocialLogin from '../SocialLogin/SocialLogin';
 import './Login.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -12,7 +17,7 @@ const Login = () => {
     const location = useLocation();
 
     let from = location.state?.from?.pathname || "/";
-
+    let errorElement;
     const [
         signInWithEmailAndPassword,
         user,
@@ -20,9 +25,19 @@ const Login = () => {
         error,
     ] = useSignInWithEmailAndPassword(auth);
 
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+
+    if(loading || sending){
+        return <Loading></Loading>
+    }
+
     if (user) {
         navigate(from, { replace: true });
     }
+
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error?.message}</p>
+        }
 
     const handleSubmit = event => {
         event.preventDefault();
@@ -36,6 +51,17 @@ const Login = () => {
         navigate('/register');
     }
 
+    const resetPassword = async() =>{
+        const email = emailRef.current.value;
+        if (email){
+            await sendPasswordResetEmail(email);
+          toast('Sent email');
+        }
+        else{
+            toast('please enter your email address');
+        }
+    }
+
     return (
         <div className='container w-50 mx-auto'>
             <h2 className='log-head mt-2'>Please login</h2>
@@ -43,24 +69,22 @@ const Login = () => {
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
+                
+                <Button variant="dark w-50 mx-auto d-block mb-2" type="submit">
+                    Login
                 </Button>
             </Form>
+            {errorElement}
             <p>New Viewer? <Link to="/register" className='text-danger pe-auto text-decoration-none' onClick={navigateRegister}>Please Register</Link></p>
-
+            <p>Forget Password? <button className='btn btn-link text-danger pe-auto text-decoration-none' onClick={resetPassword}>Reset Password</button></p>
+            <SocialLogin></SocialLogin>
+            <ToastContainer />
         </div>
     );
 };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,45 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import './Register.css';
 import auth from '../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
+import { async } from '@firebase/util';
+import Loading from '../../Shared/Loading/Loading';
 
 const Register = () => {
+    const [agree, setAgree] = useState(false);
     const [
         createUserWithEmailAndPassword,
         user,
         loading,
         error,
-      ] = useCreateUserWithEmailAndPassword(auth);
-    const navigate =useNavigate();
+    ] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification: true});
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+    const navigate = useNavigate();
 
-    const navigateLogin = () =>{
+    const navigateLogin = () => {
         navigate('/login');
     }
 
-    if(user){
-        navigate('/home');
+    if(loading || updating){
+        return <Loading></Loading>
     }
 
-    const handleRegister = event =>{
+    if (user) {
+        console.log('user', user);
+    }
+
+    const handleRegister = async (event) => {
         event.preventDefault();
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
-
-        createUserWithEmailAndPassword(email, password);
-
+        // const agree = event.target.terms.checked;
+        
+         await createUserWithEmailAndPassword(email, password);
+       await updateProfile({displayName: name});
+       console.log('Updated profile');
+       navigate('/home');
     }
     return (
         <div className='register-form'>
-             <h2 style={{textAlign: 'center'}}>please register</h2>
-             <form onSubmit={handleRegister}>
-                 <input type="text" name="name" id="" placeholder='Your Name' />
-                 <input type="email" name="email" id="" placeholder='Your Email' required />
-                 <input type="password" name="password" id="" placeholder='Your Password' required />
-                 <input type="submit" value="Register" />
-             </form>
-             <p>Already have an account? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
+            <h2 style={{ textAlign: 'center' }}>please register</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" name="name" id="" placeholder='Your Name' />
+                <input type="email" name="email" id="" placeholder='Your Email' required />
+                <input type="password" name="password" id="" placeholder='Your Password' required />
+                <input onClick={() => setAgree(!agree)} type="checkbox" name="terms" id="terms" />
+                <label className={`ps-2 ${agree ? '' : 'text-danger'}`} htmlFor="terms">Accept service provider Terms and Conditions</label>
+                <input
+                    disabled={!agree}
+                    className='w-50 mx-auto btn btn-dark mt-2'
+                    type="submit"
+                    value="Register" />
+            </form>
+            <p>Already have an account? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
